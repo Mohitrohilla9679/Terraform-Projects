58024a452721681dba189fc8a3b13174d0f9e41d
Mohit Rohilla <118180056+Mohitrohilla9679@users.noreply.github.com>
2024-02-23 16:38:11 +0530

    Update script.sh

--- Terraform-Project-1/script.sh
+++ Terraform-Project-1/script.sh
@@ -3,5 +3,5 @@ sudo apt-get update && apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
-echo "NGINX Installed Successfully" | sudo tee /var/www/html/index.html > /dev/null
+echo "NGINX Installed Successfully by Mohit Rohilla" | sudo tee /var/www/html/index.html > /dev/null
 sudo systemctl restart nginx
